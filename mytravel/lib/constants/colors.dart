593d2f07a5edd2815b9e9c8217455141ef59caf7
NirapatJK <s6503051624190@email.kmutnb.ac.th>
2025-01-28 16:10:21 +0700

    update4
    destination

---
+++ mytravel/lib/constants/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const primaryColors = Color(0xff2B3848);
+const secondaryColors = Color.fromRGBO(169, 169, 169, 1);
+
+const forestColor = Color(0xffBEE8AF);
+const campingColor = Color(0xffF5DA94);
+const boatColor = Color(0xffB3E4F1);
---
+++ mytravel/lib/models/data.dart
@@ -0,0 +1,17 @@
+class Destination {
+  late String name;
+  late String image;
+  late String rate;
+  late String location;
+
+  Destination(this.name, this.image, this.rate, this.location);
+}
+
+List<Destination> destinations = [
+  Destination("AAA", "assets/images/view1.jpg", "5.0", "Ubon, Thai"),
+  Destination("BBB", "assets/images/view2.jpg", "4.5", "Non, Thai"),
+  Destination("CCC", "assets/images/view3.jpg", "4.8", "Ubon, Thai"),
+  Destination("DDD", "assets/images/view4.jpg", "5.0", "Nakon, Thai"),
+  Destination("EEE", "assets/images/view5.jpg", "4.5", "Phuket, Thai"),
+  Destination("FFF", "assets/images/view6.jpg", "5.0", "Krabi, Thai")
+];  
